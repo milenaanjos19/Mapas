95f91f50fb7a647cb5b0adb140d6f56b2a056fa6
Milena Anjos Sigoli <milena.anjos@aluno.ifsp.edu.br>
2024-09-18 21:50:20 -0300

    inicial

---
+++ GoogleMaps.cs
@@ -0,0 +1,19 @@
+﻿namespace Mapas;
+
+public class GoogleMaps : IProvedorMapa
+{
+    public string NomeProvedor { get;} = "Google Maps";
+
+    public string Mapa(string coordenadas)
+    {
+        
+        return $"Este é um mapa de Gogle Maps para" + $"as coordenadas: {coordenadas}";
+    }
+
+    public string Rotas(string origem, string destino)
+    {
+        return $"Está a rota: {origem} e {destino}" + $"usando Google Maps";
+    }
+
+  
+}
---
+++ MapsHere.cs
@@ -0,0 +1,18 @@
+﻿namespace Mapas;
+
+public class  MapsHere : IProvedorMapa
+{
+    public string NomeProvedor { get;} = "MapsHere";
+
+    public string Mapa(string coordenadas)
+    {
+        return $"Este é um mapa de MapsHere para" + $"as coordenadas: {coordenadas}";
+    }
+
+    public string Rotas(string origem, string destino)
+    {
+        return $"Está a rota: {origem} e {destino}" + $"usando MapsHere";
+    }
+
+    
+}
